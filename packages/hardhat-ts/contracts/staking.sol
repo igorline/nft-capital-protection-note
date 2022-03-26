4304269161d6f0e1a5df3ae591ffa13b28682f9e
leon <leon.ducasse@epitech.eu>
2022-03-26 12:05:05 +0100

    [ADD] test for the staking and token contracts but with fork

--- packages/hardhat-ts/contracts/staking.sol
+++ packages/hardhat-ts/contracts/staking.sol
@@ -2,15 +2,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract contractv1 {
-  address constant USDC_ADDR = 0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9;
+  address public USDC_ADDR;
+
+  constructor(address _USDC_ADDR) {
+    USDC_ADDR = _USDC_ADDR;
+  }
 
   event TopUpDone(address indexed _from, uint256 _value);
 
   function stake(uint256 amount) public {
-    ERC20(USDC_ADDR).transferFrom(msg.sender, address(this), amount);
+    IERC20(USDC_ADDR).transferFrom(msg.sender, address(this), amount);
     emit TopUpDone(msg.sender, amount);
   }
+
+  /* function retrieveTokens(address _token) public payable {
+    require(msg.sender == owner);
+    ERC20 erctoken = ERC20(_token);
+    erctoken.transfer(Vault, erctoken.balanceOf(this));
+  } */
 }
