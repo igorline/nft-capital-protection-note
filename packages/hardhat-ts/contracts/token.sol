a19f7c97a772ab80246ca647b636be63939e58b3
leon <leon.ducasse@epitech.eu>
2022-03-26 12:05:05 +0100

    [ADD] test for the staking and token contracts but with fork

--- packages/hardhat-ts/contracts/token.sol
+++ packages/hardhat-ts/contracts/token.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract USDC is ERC20 {
-    constructor(uint256 initialSupply) ERC20("USDCOIN", "USDC") {
-        _mint(msg.sender, initialSupply);
-    }
+  constructor(uint256 initialSupply) ERC20("USDCOIN", "USDC") {
+    _mint(msg.sender, initialSupply);
+  }
 }
--- packages/hardhat-ts/test/sample-test.ts
+++ packages/hardhat-ts/test/sample-test.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { ethers } from 'hardhat';
+import { ethers, network } from 'hardhat';
 
 describe('YourContract', function () {
   it("Should return the new purpose once it's changed", async function () {
@@ -26,7 +26,7 @@ describe('YourContract', function () {
     expect(deployerBalance).to.equal(initialSupply);
   });
 
-  it('transfet proper amount of token from user to contract', async function () {
+  it('transfer proper amount of token from user to contract', async function () {
     const initialSupply = 100;
     const amount = 65;
 
@@ -37,7 +37,7 @@ describe('YourContract', function () {
     const usdcInstance = await USDC.deploy(initialSupply);
 
     const StakingContract = await ethers.getContractFactory('contractv1');
-    const stakingContract = await StakingContract.deploy();
+    const stakingContract = await StakingContract.deploy(usdcInstance.address);
 
     const allowance = await usdcInstance.allowance(deployer.address, stakingContract.address);
 
@@ -52,9 +52,48 @@ describe('YourContract', function () {
 
     const tx2 = await stakingContract.stake(amount);
     await tx2.wait();
+
     const contractBalance = await usdcInstance.balanceOf(stakingContract.address);
     const userBalance = await usdcInstance.balanceOf(deployer.address);
     expect(userBalance).to.equal(initialSupply - amount);
     expect(contractBalance).to.equal(amount);
+
+    await network.provider.request({
+      method: 'hardhat_reset',
+      params: [
+        {
+          forking: {
+            jsonRpcUrl: 'https://rpc.ankr.com/avalanche',
+            blockNumber: 12574834,
+          },
+        },
+      ],
+    });
+
+    await network.provider.request({
+      method: 'hardhat_impersonateAccount',
+      params: ['0xbf14db80d9275fb721383a77c00ae180fc40ae98'],
+    });
+
+    const signer = await ethers.getSigner('0xbf14db80d9275fb721383a77c00ae180fc40ae98');
+
+    const RealUSDC = await ethers.getContractFactory('ERC20');
+    const realUSDC = RealUSDC.attach('0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E');
+
+    const StakingContractFork = await ethers.getContractFactory('contractv1');
+    const stakingContractFork = await StakingContractFork.deploy(realUSDC.address);
+    const impStakingContract = stakingContractFork.connect(signer);
+
+    const userBalanceForkBeforeTx = await realUSDC.balanceOf(signer.address);
+
+    await (await realUSDC.connect(signer).approve(stakingContractFork.address, amount)).wait();
+    const tx3 = await impStakingContract.connect(signer).stake(amount);
+    tx3.wait();
+
+    const contractBalanceFork = await realUSDC.balanceOf(stakingContractFork.address);
+    const userBalanceFork = await realUSDC.balanceOf(signer.address);
+
+    expect(contractBalanceFork).to.equal(amount);
+    expect(userBalanceFork).to.equal(userBalanceForkBeforeTx - amount);
   });
 });
